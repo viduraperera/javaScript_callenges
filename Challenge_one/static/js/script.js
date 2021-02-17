65dd9e362d147eaa3ae9098f222246227fd3f7f4
vidura umayanaga perera <laumayangaperera@gmail.com>
2021-02-17 19:06:27 +0530

    challenge3-mid-way

--- Challenge_one/static/js/script.js
+++ Challenge_one/static/js/script.js
@@ -22,3 +22,66 @@ function genCat(){
     image.src = "https://i.imgur.com/EcZibMp.gif";
     div.appendChild(image);
 }
+
+function rpsGame(yourChoice) {
+    console.log(yourChoice);
+    var humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = numberToChoice(randomToRpsInt());
+    console.log('computer Choice: ', botChoice);
+    
+    result = decideWinner(humanChoice,botChoice);
+    console.log(result);
+
+    message = finalMsg(result); // won or lost {'msg':, 'you won':, 'green'}
+    console.log(message);
+   
+    rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randomToRpsInt(){
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number){
+    return ['rock', 'paper', 'scissors'][number]
+}
+
+function decideWinner(yourChoice, computerChoice){
+    var rpsDatabase = {
+        'rock' : {'scissors': 1, 'rock': 0.5, 'paper': 0},
+        'paper': {'scissors': 0, 'rock': 1, 'paper': 0.5},
+        'scissors':{'scissors': 0.5, 'rock': 0, 'paper': 1}
+    }
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return[yourScore, computerScore];
+}
+
+function finalMsg([yourScore, computerScore]){
+
+    if(yourScore === 0){
+        return{'message': 'you lost', 'color': 'red'};
+    }
+    else if(yourScore === 0.5){
+       return{'message': 'you tied', 'color': 'yellow'};
+    }
+    else{
+        return{'message': 'you won', 'color': 'green'};
+    }
+}
+
+function rpsFrontEnd(humanChoice, botChoice, finalmsg){
+    var imagesDataBase = {
+        'rock': document.getElementById('rock').src,
+        'paper':document.getElementById('paper').src,
+        'scissors':document.getElementById('scissors').src
+
+    }
+    //remove all the immges 
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+}
